3fca22fda73fecdba0dcab94b2e16cc7fd0aa9ba
Manny Delgado <mannygdp@gmail.com>
2023-11-12 23:08:52 -0800

    cant get page to load

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 
-
 def create_app():
     app = Flask(__name__)
 
+    # index route
     @app.route('/')
-    def hello(): 
-        return 'Hello PetFax'
-        
-    from . import pet
+    def index(): 
+        return 'Hello, Pet Facts!'
+
+    # register pet blueprint 
+    from . import pet 
     app.register_blueprint(pet.bp)
 
+    # register fact blueprint 
     from . import fact
     app.register_blueprint(fact.bp)
 
+    # return the app 
     return app
